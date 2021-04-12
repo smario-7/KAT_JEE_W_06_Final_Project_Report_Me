ad8725184a9f88567d308b77b1ef027d5eceb0cc
smario-7 <mariusz2590@gmail.com>
2021-04-13 01:04:14 +0200

    first commit

---
+++ src/main/java/coderslab/pl/reportme/ReportMeApplication.java
@@ -0,0 +1,13 @@
+package coderslab.pl.reportme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReportMeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReportMeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/coderslab/pl/reportme/ReportMeApplicationTests.java
@@ -0,0 +1,13 @@
+package coderslab.pl.reportme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReportMeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
