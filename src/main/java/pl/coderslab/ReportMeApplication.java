24fb1d5af26e4e24190d3e323042b781fafcba25
smario-7 <mariusz2590@gmail.com>
2021-09-29 08:23:51 +0200

    hide password to database

--- src/main/java/pl/coderslab/ReportMeApplication.java
+++ src/main/java/pl/coderslab/ReportMeApplication.java
@@ -1,5 +1,6 @@
 package pl.coderslab;
 
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.MessageSource;
@@ -9,6 +10,7 @@ import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 
 import javax.validation.Validator;
 
+@EnableEncryptableProperties
 @SpringBootApplication
 public class ReportMeApplication {
 
